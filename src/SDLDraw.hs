8aeae6158aced521690fd664ca8c107d652afca9
syouzan420 <syouzanponkikki@yahoo.co.jp>
2025-12-17 18:26:55 +0900

    update

--- src/SDLDraw.hs
+++ src/SDLDraw.hs
@@ -1,13 +1,18 @@
 module SDLDraw (draw) where
 
-import SDL (V4(V4),V2(V2))
+import SDL (V4(V4),V2(V2),Point(P))
 import SDL.Primitive (fillCircle)
+import SDL.Video.Renderer (copy,Rectangle(..))
 
-import WakaData (WakaData(wdRenderer,wdDouble))
+import SpriteName (SpriteName(..))
+import WakaData (WakaData(..))
 
 draw :: WakaData -> IO ()
 draw wd = do 
   let renderer = wdRenderer wd
+  let getSprite = wdGetSprite wd
+  let pli = getSprite PlayerLeft
   let i = wdDouble wd
   fillCircle renderer (V2 60 (10+fromIntegral(floor i))) 10 (V4 102 178 255 255)
+  copy renderer pli Nothing (Just (Rectangle (P (V2 10 10)) (V2 16 16)))
   putStrLn ("Hello " ++ show i)
--- src/SpriteName.hs
+++ src/SpriteName.hs
@@ -1,5 +1,11 @@
 module SpriteName where
 
 data SpriteName = PlayerFrontLeft
+                | PlayerFrontRight
+                | PlayerBackLeft
                 | PlayerBackRight
+                | PlayerLeftRight
+                | PlayerLeft
+                | PlayerRightLeft
+                | PlayerRight
          deriving (Enum,Eq,Ord,Read,Show)
